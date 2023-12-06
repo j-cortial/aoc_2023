2eb5bd4e631168b31f1143cfcac50841ed00c721
Julien Cortial <jcortial@gmail.com>
2023-12-06 22:12:58 +0100

    Solve day 06 part 2

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -7,13 +7,13 @@ use nom::{
     IResult,
 };
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Race {
-    time: i32,
-    distance: i32,
+    time: u64,
+    distance: u64,
 }
 
-fn integer(input: &str) -> IResult<&str, i32> {
+fn integer(input: &str) -> IResult<&str, u64> {
     map_res(
         recognize(many1(terminated(one_of("0123456789"), many0(char('_'))))),
         |s: &str| s.parse(),
@@ -42,12 +42,12 @@ fn parse_input(input: &str) -> Vec<Race> {
 }
 
 struct Outcome {
-    race_time: i32,
-    hold_time: i32,
+    race_time: u64,
+    hold_time: u64,
 }
 
 impl Outcome {
-    fn distance(&self) -> i32 {
+    fn distance(&self) -> u64 {
         if self.hold_time <= 0 || self.hold_time >= self.race_time {
             return 0;
         }
@@ -55,22 +55,44 @@ impl Outcome {
     }
 }
 
-fn solve_part1(races: &[Race]) -> usize {
-    races
-        .iter()
-        .map(|r| {
-            (1..r.time)
-                .map(|h| {
-                    Outcome {
-                        race_time: r.time,
-                        hold_time: h,
-                    }
-                    .distance()
-                })
-                .filter(|&d| d > r.distance)
-                .count()
+fn record_count(race: &Race) -> usize {
+    (1..race.time)
+        .map(|h| {
+            Outcome {
+                race_time: race.time,
+                hold_time: h,
+            }
+            .distance()
         })
-        .product()
+        .filter(|&d| d > race.distance)
+        .count()
+}
+
+fn solve_part1(races: &[Race]) -> usize {
+    races.iter().map(record_count).product()
+}
+
+fn join(head: u64, tail: u64) -> u64 {
+    let mut offset = 10;
+    let mut rem = tail / 10;
+    while rem > 0 {
+        offset *= 10;
+        rem /= 10;
+    }
+    tail + offset * head
+}
+
+impl Race {
+    fn collapse(races: &[Self]) -> Self {
+        races.iter().fold(Race::default(), |acc, x| Race {
+            time: join(acc.time, x.time),
+            distance: join(acc.distance, x.distance),
+        })
+    }
+}
+
+fn solve_part2(races: &[Race]) -> usize {
+    record_count(&Race::collapse(races))
 }
 
 fn main() {
@@ -78,4 +100,6 @@ fn main() {
     let races = parse_input(input);
     let answer1 = solve_part1(&races);
     println!("The answer to part 1 is {}", answer1);
+    let answer2 = solve_part2(&races);
+    println!("The answer to part 2 is {}", answer2);
 }
