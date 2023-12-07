82167c7861a87f349a64d8ed03e51ba11c6b404f
Julien Cortial <jcortial@gmail.com>
2023-12-07 20:27:44 +0100

    Solve day 07 part 1

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -1,8 +1,9 @@
 use std::str::FromStr;
 
+use counter::Counter;
 use nom::{
     character::complete::{char, multispace1, one_of, space1},
-    combinator::{map_res, recognize, map},
+    combinator::{map, map_res, recognize},
     multi::{many0, many1, many_m_n, separated_list1},
     sequence::{separated_pair, terminated},
     IResult,
@@ -10,9 +11,14 @@ use nom::{
 
 type Money = u32;
 
-#[derive(Debug)]
-struct Hand<'a> {
-    cards: &'a str,
+#[derive(Debug, Clone, Copy)]
+struct Cards(&'static str);
+
+const RANKS: &str = "23456789TJQKA";
+
+#[derive(Debug, Clone)]
+struct Hand {
+    cards: Cards,
     bet: Money,
 }
 
@@ -23,21 +29,72 @@ fn integer<I: FromStr>(input: &str) -> IResult<&str, I> {
     )(input)
 }
 
-fn parse_input(input: &str) -> Vec<Hand> {
+fn parse_input(input: &'static str) -> Vec<Hand> {
     separated_list1(
         multispace1,
-        map(separated_pair(
-            recognize(many_m_n(5, 5, one_of("AKQJT98765432"))),
-            space1,
-            integer::<Money>,
-        ), |(cards, bet)| Hand{cards, bet}),
+        map(
+            separated_pair(
+                recognize(many_m_n(5, 5, one_of(RANKS))),
+                space1,
+                integer::<Money>,
+            ),
+            |(cards, bet)| Hand {
+                cards: Cards(cards),
+                bet,
+            },
+        ),
     )(input)
     .unwrap()
     .1
 }
 
+fn count(cards: &Cards) -> Counter<char> {
+    cards.0.chars().collect()
+}
+
+fn ranks(cards: &Cards) -> Vec<usize> {
+    cards
+        .0
+        .chars()
+        .map(|c| RANKS.chars().position(|a| a == c).unwrap())
+        .collect()
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct Strength {
+    counts: Vec<usize>,
+    ranks: Vec<usize>,
+}
+
+impl Hand {
+    fn strength(&self) -> Strength {
+        Strength {
+            counts: count(&self.cards)
+                .most_common()
+                .into_iter()
+                .map(|(_, c)| c)
+                .collect(),
+            ranks: ranks(&self.cards),
+        }
+    }
+}
+
+fn solve_part1(hands: &[Hand]) -> usize {
+    let sorted_hands = {
+        let mut hands = hands.to_vec();
+        hands.sort_unstable_by_key(Hand::strength);
+        hands
+    };
+    sorted_hands
+        .into_iter()
+        .enumerate()
+        .map(|(i, h)| (i + 1) * h.bet as usize)
+        .sum()
+}
+
 fn main() {
     let input = include_str!("../../data/day07.txt");
     let hands = parse_input(input);
-    dbg!(&hands);
+    let answer1 = solve_part1(&hands);
+    println!("The answer to part 1 is {}", answer1);
 }
