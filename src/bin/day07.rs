6f3ca8c5da3e49c2be51cd7dddb28fb444544936
Julien Cortial <jcortial@gmail.com>
2023-12-07 20:51:43 +0100

    Solve day 07 part 2

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -15,6 +15,7 @@ type Money = u32;
 struct Cards(&'static str);
 
 const RANKS: &str = "23456789TJQKA";
+const ALT_RANKS: &str = "J23456789TQKA";
 
 #[derive(Debug, Clone)]
 struct Hand {
@@ -60,6 +61,14 @@ fn ranks(cards: &Cards) -> Vec<usize> {
         .collect()
 }
 
+fn alt_ranks(cards: &Cards) -> Vec<usize> {
+    cards
+        .0
+        .chars()
+        .map(|c| ALT_RANKS.chars().position(|a| a == c).unwrap())
+        .collect()
+}
+
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Strength {
     counts: Vec<usize>,
@@ -77,6 +86,27 @@ impl Hand {
             ranks: ranks(&self.cards),
         }
     }
+
+    fn alt_strength(&self) -> Strength {
+        let counts = count(&self.cards);
+        let jokers = counts.get(&'J').copied().unwrap_or_default();
+        let mut counts: Vec<_> = counts
+            .most_common()
+            .into_iter()
+            .filter_map(|(c, n)| match c {
+                'J' => None,
+                _ => Some(n),
+            })
+            .collect();
+        if counts.is_empty() {
+            counts.push(0);
+        }
+        counts[0] += jokers;
+        Strength {
+            counts,
+            ranks: alt_ranks(&self.cards),
+        }
+    }
 }
 
 fn solve_part1(hands: &[Hand]) -> usize {
@@ -92,9 +122,24 @@ fn solve_part1(hands: &[Hand]) -> usize {
         .sum()
 }
 
+fn solve_part2(hands: &[Hand]) -> usize {
+    let sorted_hands = {
+        let mut hands = hands.to_vec();
+        hands.sort_unstable_by_key(Hand::alt_strength);
+        hands
+    };
+    sorted_hands
+        .into_iter()
+        .enumerate()
+        .map(|(i, h)| (i + 1) * h.bet as usize)
+        .sum()
+}
+
 fn main() {
     let input = include_str!("../../data/day07.txt");
     let hands = parse_input(input);
     let answer1 = solve_part1(&hands);
     println!("The answer to part 1 is {}", answer1);
+    let answer2 = solve_part2(&hands);
+    println!("The answer to part 2 is {}", answer2);
 }
