2a9c9b50cba6b1e7367e4fcdf74dc7a47a4ebe84
Julien Cortial <jcortial@gmail.com>
2023-12-09 00:38:03 +0100

    Solve day 08 part 2

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -1,5 +1,4 @@
-use std::collections::HashMap;
-
+use gcd::Gcd;
 use nom::{
     bytes::complete::tag,
     character::complete::{anychar, multispace1, one_of},
@@ -7,6 +6,7 @@ use nom::{
     multi::{many1, many_m_n, separated_list1},
     sequence::{delimited, separated_pair},
 };
+use std::collections::HashMap;
 
 type Direction = char;
 type Node = &'static str;
@@ -51,9 +51,46 @@ fn solve_part1(directions: &[Direction], transitions: &HashMap<Node, (Node, Node
         .count()
 }
 
+fn lcm(first: usize, second: usize) -> usize {
+    first * second / first.gcd(second)
+}
+
+fn solve_part2(directions: &[Direction], transitions: &HashMap<Node, (Node, Node)>) -> usize {
+    let initial_states: Vec<_> = transitions
+        .keys()
+        .copied()
+        .filter(|node| node.ends_with('A'))
+        .collect();
+    let periods: Vec<Vec<_>> = initial_states
+        .into_iter()
+        .map(|initial_state| {
+            directions
+                .iter()
+                .cycle()
+                .scan(initial_state, |state, &dir| {
+                    let reached = Some(state.ends_with('Z'));
+                    let candidates = transitions.get(state).unwrap();
+                    *state = match dir {
+                        'L' => &candidates.0,
+                        'R' => &candidates.1,
+                        _ => panic!(),
+                    };
+                    reached
+                })
+                .enumerate()
+                .filter_map(|(i, b)| if b { Some(i) } else { None })
+                .take(2)
+                .collect()
+        })
+        .collect();
+    periods.into_iter().fold(1, |acc, x| lcm(acc, x[1] - x[0]))
+}
+
 fn main() {
     let input = include_str!("../../data/day08.txt");
     let (directions, transitions) = parse_input(input);
     let answer1 = solve_part1(&directions, &transitions);
     println!("The answer to part 1 is {}", answer1);
+    let answer2 = solve_part2(&directions, &transitions);
+    println!("The answer to part 2 is {}", answer2);
 }
