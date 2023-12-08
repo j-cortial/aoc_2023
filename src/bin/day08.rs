957dd685e1f1b7556e3c101a91bf87d5ab335891
Julien Cortial <jcortial@gmail.com>
2023-12-08 23:40:37 +0100

    Solve day 08 part  1

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
+
 use nom::{
     bytes::complete::tag,
     character::complete::{anychar, multispace1, one_of},
-    combinator::recognize,
+    combinator::{map, recognize},
     multi::{many1, many_m_n, separated_list1},
     sequence::{delimited, separated_pair},
 };
@@ -9,27 +11,49 @@ use nom::{
 type Direction = char;
 type Node = &'static str;
 
-fn parse_input(input: &'static str) -> (Vec<Direction>, Vec<(Node, (Node, Node))>) {
+fn parse_input(input: &'static str) -> (Vec<Direction>, HashMap<Node, (Node, Node)>) {
     let node = |s| recognize(many_m_n(3, 3, anychar::<&str, ()>))(s);
     separated_pair(
         many1(one_of("LR")),
         multispace1,
-        separated_list1(
-            tag("\n"),
-            separated_pair(
-                node,
-                tag(" = "),
-                delimited(tag("("), separated_pair(node, tag(", "), node), tag(")")),
+        map(
+            separated_list1(
+                tag("\n"),
+                separated_pair(
+                    node,
+                    tag(" = "),
+                    delimited(tag("("), separated_pair(node, tag(", "), node), tag(")")),
+                ),
             ),
+            |list| list.into_iter().collect(),
         ),
     )(input)
     .unwrap()
     .1
 }
 
+fn solve_part1(directions: &[Direction], transitions: &HashMap<Node, (Node, Node)>) -> usize {
+    directions
+        .iter()
+        .cycle()
+        .scan("AAA", |state, &dir| {
+            if state == &"ZZZ" {
+                return None;
+            }
+            let candidates = transitions.get(state).unwrap();
+            *state = match dir {
+                'L' => &candidates.0,
+                'R' => &candidates.1,
+                _ => panic!(),
+            };
+            Some(*state)
+        })
+        .count()
+}
+
 fn main() {
     let input = include_str!("../../data/day08.txt");
     let (directions, transitions) = parse_input(input);
-    dbg!(&transitions);
-    dbg!(&directions);
+    let answer1 = solve_part1(&directions, &transitions);
+    println!("The answer to part 1 is {}", answer1);
 }
