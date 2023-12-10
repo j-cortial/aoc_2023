aea234cf7c31dcfb60cade599d825137f4f356e2
Julien Cortial <jcortial@gmail.com>
2023-12-10 23:06:03 +0100

    Solve day 10 part 2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -107,7 +107,7 @@ fn parse_input(input: &str) -> (Grid, Loc) {
     (Grid::new(tiles, start), start)
 }
 
-fn find_circuit (grid: &Grid, start: Loc) -> HashMap<Loc, usize> {
+fn find_circuit(grid: &Grid, start: Loc) -> HashMap<Loc, usize> {
     let mut distances = HashMap::from([(start, 0)]);
     let mut front: Vec<_> = grid
         .tile(start)
@@ -141,6 +141,82 @@ fn solve_part1(circuit: &HashMap<Loc, usize>) -> usize {
     circuit.values().copied().max().unwrap()
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Status {
+    Out,
+    In,
+    InWall,
+    InIfNorth,
+    InIfSouth,
+}
+
+impl Status {
+    fn next(&self, tile: Tile) -> Self {
+        use Status::*;
+        match *self {
+            Out => match tile {
+                Tile::Ground => panic!(),
+                Tile::NorthSouth => InWall,
+                Tile::EastWest => panic!(),
+                Tile::NorthEast => InIfSouth,
+                Tile::NorthWest => panic!(),
+                Tile::SouthWest => panic!(),
+                Tile::SouthEast => InIfNorth,
+            },
+            In | InWall => match tile {
+                Tile::Ground => panic!(),
+                Tile::NorthSouth => Out,
+                Tile::EastWest => panic!(),
+                Tile::NorthEast => InIfNorth,
+                Tile::NorthWest => panic!(),
+                Tile::SouthWest => panic!(),
+                Tile::SouthEast => InIfSouth,
+            },
+            InIfNorth => match tile {
+                Tile::Ground => panic!(),
+                Tile::NorthSouth => panic!(),
+                Tile::EastWest => *self,
+                Tile::NorthEast => panic!(),
+                Tile::NorthWest => InWall,
+                Tile::SouthWest => Out,
+                Tile::SouthEast => panic!(),
+            },
+            InIfSouth => match tile {
+                Tile::Ground => panic!(),
+                Tile::NorthSouth => panic!(),
+                Tile::EastWest => *self,
+                Tile::NorthEast => panic!(),
+                Tile::NorthWest => Out,
+                Tile::SouthWest => InWall,
+                Tile::SouthEast => panic!(),
+            },
+        }
+    }
+}
+
+fn solve_part2(grid: &Grid, circuit: &HashMap<Loc, usize>) -> usize {
+    grid.tiles
+        .iter()
+        .enumerate()
+        .map(|(i, row)| {
+            row.iter()
+                .enumerate()
+                .map(move |(j, tile)| ((i as i64, j as i64), tile))
+                .scan(Status::Out, |acc, (loc, tile)| {
+                    *acc = if circuit.contains_key(&loc) {
+                        acc.next(*tile)
+                    } else if *acc == Status::InWall {
+                        Status::In
+                    } else {
+                        *acc
+                    };
+                    Some(*acc)
+                })
+                .filter(|&status| status == Status::In)
+                .count()
+        })
+        .sum()
+}
 
 fn main() {
     let input = include_str!("../../data/day10.txt");
@@ -148,4 +224,6 @@ fn main() {
     let circuit = find_circuit(&grid, start);
     let answer1 = solve_part1(&circuit);
     println!("The answer to part 1 is {}", answer1);
+    let answer2 = solve_part2(&grid, &circuit);
+    println!("The answer to part 2 is {}", answer2);
 }
