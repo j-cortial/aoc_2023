5a3a9f1bf4d52ea8877a9cca1d65c2b80245dc55
Julien Cortial <jcortial@gmail.com>
2023-12-15 00:54:43 +0100

    Solve day 14 part 2

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,7 +1,8 @@
 use itertools::Itertools;
-use std::iter::repeat;
+use std::collections::{hash_map::Entry, HashMap};
+use std::iter::{repeat, successors};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Tile {
     Empty,
     Stable,
@@ -30,37 +31,60 @@ fn roll_to_begin(line: impl Iterator<Item = Tile>) -> Vec<Tile> {
         .collect()
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Platform {
     tiles: Vec<Vec<Tile>>,
 }
 
 impl Platform {
-    fn row_count(&self) -> usize {
-        self.tiles.len()
-    }
-
     fn col_count(&self) -> usize {
         self.tiles[0].len()
     }
 
-    fn roll_north(&self) -> Self {
-        let transposed: Vec<Vec<_>> = (0..self.col_count())
-            .map(|j| roll_to_begin(self.tiles.iter().map(move |row| row[j])))
-            .collect();
-
-        Platform {
-            tiles: (0..self.row_count())
-                .map(|i| transposed.iter().map(move |col| col[i]).collect())
+    fn turn_anticlockwise(&self) -> Self {
+        Self {
+            tiles: (0..self.col_count())
+                .rev()
+                .map(|j| self.tiles.iter().map(move |row| row[j]).collect())
                 .collect(),
         }
     }
 
-    fn load_on_north_beam(&self) -> u64 {
+    fn turn_clockwise(&self) -> Self {
+        Self {
+            tiles: (0..self.col_count())
+                .map(|j| self.tiles.iter().rev().map(move |row| row[j]).collect())
+                .collect(),
+        }
+    }
+
+    fn roll_left(&self) -> Self {
+        Self {
+            tiles: self
+                .tiles
+                .iter()
+                .map(|row| roll_to_begin(row.iter().copied()))
+                .collect(),
+        }
+    }
+
+    fn cycle_once(self) -> (Self, Self) {
+        let steps: Vec<_> = successors(Some(self), |curr| Some(curr.roll_left().turn_clockwise()))
+            .take(5)
+            .collect();
+        let mut iter = steps.into_iter();
+        (iter.next().unwrap(), iter.last().unwrap())
+    }
+
+    fn load_on_left_beam(&self) -> u64 {
         self.tiles
             .iter()
-            .rev()
-            .enumerate()
-            .map(|(i, row)| (i + 1) * row.iter().filter(|&&tile| tile == Tile::Round).count())
+            .flat_map(|row| {
+                row.iter()
+                    .rev()
+                    .enumerate()
+                    .filter_map(|(i, &tile)| (tile == Tile::Round).then_some(i + 1))
+            })
             .sum::<usize>() as u64
     }
 }
@@ -84,7 +108,36 @@ fn parse_input(input: &str) -> Platform {
 }
 
 fn solve_part1(platform: &Platform) -> u64 {
-    platform.roll_north().load_on_north_beam()
+    platform
+        .turn_anticlockwise()
+        .roll_left()
+        .load_on_left_beam()
+}
+
+fn solve_part2(platform: &Platform) -> u64 {
+    let mut curr = platform.turn_anticlockwise();
+    let mut memory = HashMap::new();
+    let mut j: usize = 0;
+    let i = loop {
+        let (init, next) = curr.cycle_once();
+        match memory.entry(init) {
+            Entry::Occupied(entry) => {
+                let (init, i) = entry.remove_entry();
+                curr = init;
+                break i;
+            }
+            Entry::Vacant(entry) => entry.insert(j),
+        };
+        curr = next;
+        j += 1;
+    };
+    let remainder = (1_000_000_000 - j) % (j - i);
+    for _ in 0..remainder {
+        let (_, next) = curr.cycle_once();
+        curr = next;
+        j += 1;
+    }
+    curr.load_on_left_beam()
 }
 
 fn main() {
@@ -92,4 +145,6 @@ fn main() {
     let platform = parse_input(input);
     let answer1 = solve_part1(&platform);
     println!("The answer for part 1 is {}", answer1);
+    let answer2 = solve_part2(&platform);
+    println!("The answer for part 2 is {}", answer2);
 }
