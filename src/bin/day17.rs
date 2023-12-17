87a73c1b77f65f7ba9e9c0935c00252daaf838ce
Julien Cortial <jcortial@gmail.com>
2023-12-17 22:26:19 +0100

    Solve day 17 part 2

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -1,4 +1,4 @@
-use std::collections::{BinaryHeap, HashSet};
+use std::collections::{BinaryHeap, HashMap};
 
 #[derive(Debug)]
 struct City {
@@ -39,7 +39,7 @@ impl Loc {
     }
 
     fn manhattan_distance(self, other: Self) -> i16 {
-        (self.0 - other.0).abs() + (self.1 + other.1).abs()
+        (self.0 - other.0).abs() + (self.1 - other.1).abs()
     }
 }
 
@@ -82,10 +82,15 @@ impl Direction {
 }
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
-struct Status<const N: u8, const M: u8> {
+struct PhysicalStatus {
     loc: Loc,
     dir: Direction,
     repeats: u8,
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+struct Status<const N: u8, const M: u8> {
+    physical_status: PhysicalStatus,
     heat_loss: u16,
     heuristic: u16,
 }
@@ -97,20 +102,22 @@ impl<const N: u8, const M: u8> Status<N, M> {
         target: Loc,
     ) -> impl Iterator<Item = Status<N, M>> + 'a {
         Direction::all()
-            .filter(|&d| d != self.dir.opposite())
-            .filter(|&d| self.repeats >= N || d == self.dir)
-            .filter(|&d| self.repeats < M || d != self.dir)
+            .filter(|&d| d != self.physical_status.dir.opposite())
+            .filter(|&d| self.physical_status.repeats >= N || d == self.physical_status.dir)
+            .filter(|&d| self.physical_status.repeats < M || d != self.physical_status.dir)
             .filter_map(move |d| {
-                let next_loc = self.loc.shift(d);
+                let next_loc = self.physical_status.loc.shift(d);
                 city.block(next_loc).map(|loss| Status {
-                    loc: next_loc,
-                    dir: d,
-                    repeats: {
-                        if d == self.dir {
-                            self.repeats + 1
-                        } else {
-                            1
-                        }
+                    physical_status: PhysicalStatus {
+                        loc: next_loc,
+                        dir: d,
+                        repeats: {
+                            if d == self.physical_status.dir {
+                                self.physical_status.repeats + 1
+                            } else {
+                                1
+                            }
+                        },
                     },
                     heat_loss: self.heat_loss + loss as u16,
                     heuristic: next_loc.manhattan_distance(target) as u16,
@@ -132,30 +139,34 @@ impl<const N: u8, const M: u8> Ord for Status<N, M> {
 }
 
 fn solve<const N: u8, const M: u8>(city: &City) -> u16 {
-    let mut visited = HashSet::new();
+    let mut visited = HashMap::new();
     let mut front = BinaryHeap::new();
     let target = Loc(city.row_count() as i16 - 1, city.col_count() as i16 - 1);
     [Direction::East, Direction::South]
         .into_iter()
         .map(|d| Status::<N, M> {
-            loc: Loc(0, 0),
-            dir: d,
-            repeats: 0,
+            physical_status: PhysicalStatus {
+                loc: Loc(0, 0),
+                dir: d,
+                repeats: 0,
+            },
             heat_loss: 0,
             heuristic: Loc(0, 0).manhattan_distance(target) as u16,
         })
         .collect::<Vec<_>>()
         .into_iter()
         .for_each(|init| {
-            visited.insert(init);
+            visited.insert(init.physical_status, 0u16);
             front.push(init);
         });
     while let Some(status) = front.pop() {
-        if status.loc == target {
+        if status.physical_status.loc == target  && status.physical_status.repeats >= N {
             return status.heat_loss;
         }
         for neighbor in status.neighbors(&city, target) {
-            if visited.insert(neighbor) {
+            let entry = visited.entry(neighbor.physical_status).or_insert(u16::MAX);
+            if status.heat_loss < *entry {
+                *entry = status.heat_loss;
                 front.push(neighbor);
             }
         }
@@ -221,5 +232,16 @@ mod test {
     fn test_solve_part2() {
         let city = parse_input(INPUT);
         assert_eq!(solve_part2(&city), 94);
+    }
+
+    #[test]
+    fn test_solve_part2_alt() {
+        let input = "111111111111
+999999999991
+999999999991
+999999999991
+999999999991";
+        let city = parse_input(input);
+        assert_eq!(solve_part2(&city), 71);
     }
 }
