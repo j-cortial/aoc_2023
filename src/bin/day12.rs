8ee71ffeb61424e367266c43b688d599564cd899
Julien Cortial <jcortial@gmail.com>
2023-12-28 23:22:10 +0100

    Solve day 12 part 1

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,5 +1,6 @@
 use std::str::FromStr;
 
+use itertools::Itertools;
 use nom::{
     character::complete::{anychar, char, newline, one_of, space1},
     combinator::{map, map_res, recognize},
@@ -7,8 +8,9 @@ use nom::{
     sequence::{separated_pair, terminated},
     IResult,
 };
+use strum::EnumIs;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, EnumIs)]
 enum Condition {
     Operational,
     Damaged,
@@ -28,6 +30,28 @@ impl TryFrom<char> for Condition {
     }
 }
 
+fn checksum(conditions: &[Condition]) -> Option<Vec<u8>> {
+    if conditions.contains(&Condition::Unknown) {
+        return None;
+    }
+    Some(
+        conditions
+            .split(|condition| condition.is_operational())
+            .map(|slice| slice.len() as u8)
+            .filter(|&len| len != 0)
+            .collect(),
+    )
+}
+
+fn restore(conditions: &[Condition], guess: &[Condition]) -> Vec<Condition> {
+    let mut res = conditions.to_vec();
+    res.iter_mut()
+        .filter(|condition| condition.is_unknown())
+        .zip_eq(guess.iter())
+        .for_each(|(c, &g)| *c = g);
+    res
+}
+
 #[derive(Debug)]
 struct Record {
     conditions: Vec<Condition>,
@@ -39,6 +63,29 @@ fn integer<I: FromStr>(input: &str) -> IResult<&str, I> {
         recognize(many1(terminated(one_of("0123456789"), many0(char('_'))))),
         |s: &str| s.parse(),
     )(input)
+}
+
+impl Record {
+    fn valid_combinations<'a>(&'a self) -> impl Iterator<Item = Vec<Condition>> + 'a {
+        let missing_damaged_count = self.checksum.iter().sum::<u8>() as usize
+            - self.conditions.iter().filter(|c| c.is_damaged()).count();
+        let unknown_count = self.conditions.iter().filter(|c| c.is_unknown()).count();
+        (0..unknown_count)
+            .combinations(missing_damaged_count)
+            .map(move |indices| {
+                let guess: Vec<_> = (0..unknown_count)
+                    .map(|i| {
+                        if indices.contains(&i) {
+                            Condition::Damaged
+                        } else {
+                            Condition::Operational
+                        }
+                    })
+                    .collect();
+                restore(&self.conditions, &guess)
+            })
+            .filter(|candidate| &checksum(candidate).unwrap() == &self.checksum)
+    }
 }
 
 fn parse_input(input: &str) -> Vec<Record> {
@@ -60,8 +107,88 @@ fn parse_input(input: &str) -> Vec<Record> {
     .1
 }
 
+fn solve_part1(records: &[Record]) -> usize {
+    records
+        .iter()
+        .map(|record| record.valid_combinations().count())
+        .sum()
+}
+
 fn main() {
     let input = include_str!("../../data/day12.txt");
     let records = parse_input(input);
-    dbg!(&records);
+    let answer1 = solve_part1(&records);
+    println!("The answer to part 1 is {}", answer1);
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn test_checksum() {
+        use Condition::*;
+        assert_eq!(
+            checksum(&[
+                Operational,
+                Damaged,
+                Operational,
+                Operational,
+                Damaged,
+                Damaged,
+                Operational
+            ]),
+            Some(vec![1, 2])
+        );
+    }
+
+    #[test]
+    fn test_restore() {
+        use Condition::*;
+        assert_eq!(
+            restore(
+                &[
+                    Operational,
+                    Damaged,
+                    Unknown,
+                    Unknown,
+                    Unknown,
+                    Damaged,
+                    Unknown
+                ],
+                &[Operational, Operational, Damaged, Operational]
+            ),
+            vec![
+                Operational,
+                Damaged,
+                Operational,
+                Operational,
+                Damaged,
+                Damaged,
+                Operational
+            ]
+        );
+    }
+
+    #[test]
+    fn test_valid_combinations() {
+        use Condition::*;
+        assert_eq!(
+            Record {
+                conditions: vec![
+                    Unknown,
+                    Unknown,
+                    Unknown,
+                    Operational,
+                    Damaged,
+                    Damaged,
+                    Damaged,
+                ],
+                checksum: vec![1, 1, 3],
+            }
+            .valid_combinations()
+            .count(),
+            1
+        );
+    }
 }
