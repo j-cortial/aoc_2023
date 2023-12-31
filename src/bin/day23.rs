bf47c14f25f28e553d2de0713b2d0950a2f530fe
Julien Cortial <jcortial@gmail.com>
2023-12-31 18:22:12 +0100

    Solve day 23 part 2

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -65,6 +65,51 @@ impl Terrain {
     fn exit(&self) -> Loc {
         [self.rows - 1, self.cols - 2]
     }
+
+    fn valid_dry_moves_from<'a>(&'a self, loc: &'a Loc) -> impl Iterator<Item = (Dir, Loc)> + 'a {
+        Dir::iter()
+            .map(|dir| (dir, dir.offset(loc)))
+            .filter(|(_, next_loc)| self.tiles.contains_key(next_loc))
+    }
+
+    fn dry_nodes(&self) -> HashMap<Loc, Vec<Dir>> {
+        self.tiles
+            .keys()
+            .map(|loc| {
+                (
+                    *loc,
+                    self.valid_dry_moves_from(loc)
+                        .map(|(d, _)| d)
+                        .collect::<Vec<_>>(),
+                )
+            })
+            .filter(|(_, v)| v.len() != 2)
+            .collect()
+    }
+
+    fn dry_edges(&self) -> HashMap<Loc, Vec<(Dir, Loc, u64)>> {
+        let nodes = self.dry_nodes();
+        let mut res = HashMap::<Loc, Vec<_>>::new();
+        for (node_loc, node_dirs) in nodes.iter() {
+            for dir in node_dirs {
+                let mut weight = 1;
+                let mut prev_dir = *dir;
+                let mut curr_loc = prev_dir.offset(node_loc);
+                while !nodes.contains_key(&curr_loc) {
+                    (prev_dir, curr_loc) = self
+                        .valid_dry_moves_from(&curr_loc)
+                        .filter(|(d, _)| *d != prev_dir.opposite())
+                        .next()
+                        .unwrap();
+                    weight += 1;
+                }
+                res.entry(*node_loc)
+                    .or_default()
+                    .push((*dir, curr_loc, weight));
+            }
+        }
+        res
+    }
 }
 
 fn parse_input(input: &str) -> Terrain {
@@ -187,8 +232,44 @@ fn solve_part1(terrain: &Terrain) -> u64 {
     solve(terrain, false)
 }
 
+fn solve_impl(
+    edges: &HashMap<Loc, Vec<(Dir, Loc, u64)>>,
+    goal: Loc,
+    mut visited: HashSet<Loc>,
+    node: Loc,
+    path_length: u64,
+    res: &mut u64,
+) {
+    visited.insert(node);
+    for &(_, next_node, weight) in &edges[&node] {
+        if !visited.contains(&next_node) {
+            if next_node == goal {
+                *res = (*res).max(path_length + weight);
+            } else {
+                solve_impl(
+                    edges,
+                    goal,
+                    visited.clone(),
+                    next_node,
+                    path_length + weight,
+                    res,
+                );
+            }
+        }
+    }
+}
+
 fn solve_part2(terrain: &Terrain) -> u64 {
-    solve(terrain, true)
+    let mut res = 0;
+    solve_impl(
+        &terrain.dry_edges(),
+        terrain.exit(),
+        Default::default(),
+        terrain.entry(),
+        0,
+        &mut res,
+    );
+    res
 }
 
 fn main() {
