c5f002865905540f3e24421c87602ffb2bac74c9
Julien Cortial <jcortial@gmail.com>
2024-01-05 22:40:51 +0100

    Solve day 25 part 1

--- src/bin/day25.rs
+++ src/bin/day25.rs
@@ -1,3 +1,10 @@
+use std::{
+    collections::{HashMap, HashSet},
+    iter::once,
+};
+
+use rustworkx_core::{connectivity::stoer_wagner_min_cut, petgraph::graph::UnGraph};
+
 fn parse_input(input: &str) -> Vec<(&str, Vec<&str>)> {
     input
         .lines()
@@ -9,8 +16,29 @@ fn parse_input(input: &str) -> Vec<(&str, Vec<&str>)> {
         .collect()
 }
 
+fn solve_part1(data: &[(&str, Vec<&str>)]) -> usize {
+    let labels: HashSet<_> = data
+        .iter()
+        .flat_map(|(a, bs)| once(a).chain(bs.iter()).copied())
+        .collect();
+    let mut graph: UnGraph<&str, ()> = UnGraph::new_undirected();
+    let nodes: HashMap<_, _> = labels
+        .iter()
+        .map(|label| (*label, graph.add_node(*label)))
+        .collect();
+    data.iter().for_each(|(start, ends)| {
+        graph.extend_with_edges(ends.iter().map(|end| (nodes[*start], nodes[*end])));
+    });
+    let min_cut_res: Result<Option<(usize, Vec<_>)>, ()> = stoer_wagner_min_cut(&graph, |_| Ok(1));
+
+    let (min_cut, partition) = min_cut_res.unwrap().unwrap();
+    assert_eq!(min_cut, 3);
+    partition.len() * (labels.len() - partition.len())
+}
+
 fn main() {
     let input = include_str!("../../data/day25.txt");
     let data = parse_input(input);
-    dbg!(&data);
+    let answer1 = solve_part1(&data);
+    println!("The answer to part 1 is {answer1}");
 }
