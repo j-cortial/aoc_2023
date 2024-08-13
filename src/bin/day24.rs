aaa41c9a632fcfbeabc63f25e64161013202d999
Julien Cortial <jcortial@gmail.com>
2024-08-13 17:39:21 +0200

    Convert day 24 to nalgebra

--- src/bin/day24.rs
+++ src/bin/day24.rs
@@ -1,54 +1,17 @@
-use std::ops::{Add, Deref, Sub};
-
 use itertools::Itertools;
+
+use nalgebra::{Const, OMatrix, Vector3};
 
 type Coord = i64;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Loc<const D: usize> {
-    data: [Coord; D],
+type Loc<const D: usize> = OMatrix<Coord, Const<D>, Const<1>>;
+
+fn projection(loc2: &Loc<3>) -> Loc<2> {
+    loc2.fixed_rows::<2>(0).into()
 }
 
-impl<const D: usize> Loc<D> {
-    fn new(data: [Coord; D]) -> Self {
-        Self { data }
-    }
-}
-
-impl<const D: usize> Deref for Loc<D> {
-    type Target = [Coord; D];
-
-    fn deref(&self) -> &Self::Target {
-        &self.data
-    }
-}
-
-impl Add for Loc<2> {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Self::new([self[0] + rhs[0], self[1] + rhs[1]])
-    }
-}
-
-impl Sub for Loc<2> {
-    type Output = Self;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        Self::new([self[0] - rhs[0], self[1] - rhs[1]])
-    }
-}
-
-impl Loc<2> {
-    fn cross_product(self, rhs: Self) -> Coord {
-        self[0] * rhs[1] - self[1] * rhs[0]
-    }
-}
-
-impl Loc<3> {
-    fn projection(self) -> Loc<2> {
-        Loc::new([self[0], self[1]])
-    }
+fn cross_product(left: &Loc<2>, right: &Loc<2>) -> Coord {
+    left[0] * right[1] - left[1] * right[0]
 }
 
 #[derive(Debug)]
@@ -60,23 +23,23 @@ struct Hailstone<const D: usize> {
 impl Hailstone<3> {
     fn projection(&self) -> Hailstone<2> {
         Hailstone {
-            position: self.position.projection(),
-            velocity: self.velocity.projection(),
+            position: projection(&self.position),
+            velocity: projection(&self.velocity),
         }
     }
 }
 
 fn future_intersection(a: &Hailstone<2>, b: &Hailstone<2>) -> Option<[f64; 2]> {
-    let det = b.velocity.cross_product(a.velocity);
+    let det = cross_product(&b.velocity, &a.velocity);
     if det == 0 {
         return None;
     }
     let relative_position = b.position - a.position;
-    let a_time = b.velocity.cross_product(relative_position);
+    let a_time = cross_product(&b.velocity, &relative_position);
     if a_time.signum() != det.signum() {
         return None;
     }
-    let b_time = a.velocity.cross_product(relative_position);
+    let b_time = cross_product(&a.velocity, &relative_position);
     if b_time.signum() != det.signum() {
         return None;
     }
@@ -93,7 +56,7 @@ fn in_range<const LOWER_BOUND: Coord, const UPPER_BOUND: Coord>(coord: f64) -> b
 fn parse_loc3(input: &str) -> Loc<3> {
     let mut iter = input.split(", ");
     let mut parse = || iter.next().map(|s| s.trim().parse().unwrap()).unwrap();
-    Loc::new([parse(), parse(), parse()])
+    Vector3::new(parse(), parse(), parse())
 }
 
 fn parse_input(input: &str) -> Vec<Hailstone<3>> {
